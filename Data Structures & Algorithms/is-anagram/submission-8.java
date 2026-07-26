5dfd7256fb341e3058e89d4721c18d4dbfbd8445
Aditya95233 <145892125+Aditya95233@users.noreply.github.com>
2026-07-26 21:42:27 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    //    brute force
+        if(s.length()!=t.length()) return false;
+        // char[] temp1=s.toCharArray();
+        // char[] temp2=t.toCharArray();
+        // Arrays.sort(temp1);
+        // Arrays.sort(temp2);
+        // for(int i=0;i<temp1.length;i++){
+        //     if(temp1[i]!=temp2[i]) return false;
+        // }
+        // return true;
+// =================================================
+    //  better approach
+        // int[] res=new int[(int)'z'+1];
+        // for(int i=0;i<s.length();i++){
+        //     int crrIndadd=s.charAt(i);
+        //     res[crrIndadd]+=1;
+        //     int crrIndremove=t.charAt(i);
+        //     res[crrIndremove]-=1;
+        // }
+        // for(int i=0;i<res.length;i++){
+        //     if(res[i]!=0) return false;
+        // }
+        // return true;
+// ==================================================
+    int[] res=new int[26];
+    for(int i=0;i<s.length();i++){
+        int addInd=(int)s.charAt(i)-(int)'a';
+        res[addInd]+=1;
+        int removeInd=(int)t.charAt(i)-(int)'a';
+        res[removeInd]-=1;
+    }
+    for(int i=0;i<26;i++){
+        if(res[i]!=0) return false;
+    }
+    return true;
+    }
+}
